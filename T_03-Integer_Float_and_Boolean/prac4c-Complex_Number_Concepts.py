9905fa4d377090306de948aab427377ddb452d8b
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2026-05-10 09:48:08 +0600

    Added complex number operations

---
+++ T_03-Integer_Float_and_Boolean/prac4c-Complex_Number_Concepts.py
@@ -0,0 +1,56 @@
+# Complex Numbers
+
+z = 3 + 4j
+
+print(z)
+print(z.real)
+print(z.imag)
+
+
+# Complex Number Addition
+
+a = 2 + 3j
+b = 1 + 4j
+
+print(a + b)
+
+
+# Floating Point Precision
+
+print(0.1 + 0.2)
+
+
+# Decimal Module Basics
+
+from decimal import Decimal
+
+a = Decimal("0.1")
+b = Decimal("0.2")
+
+print(a + b)
+
+
+# Number Base Conversion
+
+num = 25
+
+print(bin(num))
+print(oct(num))
+print(hex(num))
+
+
+# ASCII Value Conversion
+
+print(ord("A"))
+print(chr(65))
+
+
+# Checking Number Type
+
+a = 10
+b = 10.5
+c = 3 + 2j
+
+print(type(a))
+print(type(b))
+print(type(c))
