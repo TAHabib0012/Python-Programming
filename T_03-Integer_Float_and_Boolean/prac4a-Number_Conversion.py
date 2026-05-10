2586a9a6041dcd8f44429ea1657f10b3986d4419
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2026-05-10 08:28:23 +0600

    Added  numeral operations

---
+++ T_03-Integer_Float_and_Boolean/prac4a-Number_Conversion.py
@@ -0,0 +1,46 @@
+# Binary Numbers
+
+num = 10
+
+print(bin(num))
+
+
+# Binary to Decimal
+
+binary_num = "1010"
+
+decimal_num = int(binary_num, 2)
+
+print(decimal_num)
+
+
+# Hexadecimal Numbers
+
+num = 255
+
+print(hex(num))
+
+
+# Hexadecimal to Decimal
+
+hex_num = "FF"
+
+decimal_num = int(hex_num, 16)
+
+print(decimal_num)
+
+
+# Octal Numbers
+
+num = 20
+
+print(oct(num))
+
+
+# Octal to Decimal
+
+octal_num = "24"
+
+decimal_num = int(octal_num, 8)
+
+print(decimal_num)
