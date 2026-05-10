2ed615206085dab9d0271d2968b8fb20aa1673c6
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2026-05-10 09:46:20 +0600

    Added bitwise operations

---
+++ T_03-Integer_Float_and_Boolean/prac4b-Bitwise_Operations.py
@@ -0,0 +1,31 @@
+# Bitwise AND
+
+a = 5
+b = 3
+
+print(a & b)
+
+
+# Bitwise OR
+
+print(a | b)
+
+
+# Bitwise XOR
+
+print(a ^ b)
+
+
+# Bitwise NOT
+
+print(~a)
+
+
+# Left Shift
+
+print(a << 1)
+
+
+# Right Shift
+
+print(a >> 1)
